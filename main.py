99fe8d2881ff3fe5e6f0dbbe1b20c5fd49eca2eb
AsimSahoo <>
2021-08-27 03:51:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import music
+from keep_alive import keep_alive
+
+cogs = [music]
+
+client = commands.Bot(command_prefix='-', intents=discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+keep_alive()
+client.run("ODgwNTA1NjcyMDQ1ODI2MTEw.YSfQzw.gZgsOtMejSZiNAFQKs1eDT0h0Xo")
+
